70a63bbecdb077c945fa663f6b9fba21b5320d66
vfasky <vfasky@gmail.com>
2015-03-31 22:51:13 +0800

    更改 select label 值读取规则

--- dist/rvsui.all.js
+++ dist/rvsui.all.js
@@ -371,26 +371,26 @@ define('rvsui/route', [], function() {
 
 ;
 /**
- * 下拉框 
+ * 下拉框
  * @date 2015-03-30 23:20:55
  * @author vfasky <vfasky@gmail.com>
  */
 
-define('rvsui/select', ['jquery', 'rvsui/widgetBase'], function($, Widget){
+define('rvsui/select', ['jquery', 'rvsui/widgetBase'], function($, Widget) {
     "use strict";
-    
+
     var $win = $(window);
 
     Widget.reg('select', Widget.subclass({
         constructor: Widget.prototype.constructor,
-        init: function(){
+        init: function() {
             this.$soure.hide();
             this.isStatic = this.$soure.attr('data-static') !== undefined;
             this.$el = $('<div class="ui selection dropdown" />');
             this.$label = $('<div class="text" />').appendTo(this.$el);
             this.$el.append('<i class="dropdown icon" />');
             this.$list = $('<div class="menu transition hidden" tabindex="-1" />').appendTo(this.$el);
-            this.$label.text(this.$soure.attr('placeholder') || '');
+            this.$label.text(this.$soure.attr('title') || this.$soure.attr('placeholder') || '');
 
             this.list = null;
             this.syncList();
@@ -402,78 +402,78 @@ define('rvsui/select', ['jquery', 'rvsui/widgetBase'], function($, Widget){
          *
          * @return {Array}
          */
-        syncList: function(){
+        syncList: function() {
             var self = this;
-            if(null === this.list || false === this.isStatic){
+            if (null === this.list || false === this.isStatic) {
                 this.list = [];
                 this.$list.find('.item').remove();
                 var html = '';
-                
-                this.$soure.find('option').each(function(){
+
+                this.$soure.find('option').each(function() {
                     var $el = $(this);
                     var item = {
                         value: $el.val(),
                         text: $el.text()
                     };
-                    html += '<div class="item" data-value="'+ item.value +'">'+
-                                item.text +
-                            '</div>';
+                    html += '<div class="item" data-value="' + item.value + '">' +
+                        item.text +
+                        '</div>';
 
                     self.list.push(item);
                 });
 
                 this.$list.html(html);
             }
-            
+
             return this.list;
         },
-        update: function(value){
+        update: function(value) {
             this.$soure.val(value);
 
             this.$list.find('.selected').attr('class', 'item');
-            if(value){
+            if (value) {
                 var $el = this.$list.find('[data-value=' + value + ']')
-                              .attr('class', 'item active selected');
+                    .attr('class', 'item active selected');
 
-                if($el.length > 0){
+                if ($el.length > 0) {
                     this.$label.text($el.text());
                 }
             }
         },
-        showList: function(){
+        showList: function() {
             this.$el.attr('class', 'ui selection dropdown active visible');
             this.$list.attr('class', 'menu transition visible');
         },
-        hideList: function(){
+        hideList: function() {
             this.$el.attr('class', 'ui selection dropdown');
             this.$list.attr('class', 'menu transition hidden');
         },
-        watch: function(){
+        watch: function() {
             var self = this;
-            this.$el.on('click',  function(){
+            this.$el.on('click', function() {
                 self.showList();
                 self.syncList();
 
-                if(false === self.isStatic){
+                if (false === self.isStatic) {
                     self.update(self.getValue());
                 }
 
                 return false;
-            }).on('click', '.item', function(){
+            }).on('click', '.item', function() {
                 var $el = $(this);
                 var value = $el.attr('data-value');
                 self.sync(value);
                 self.hideList();
                 self.$soure.trigger('change');
-                
+
                 return false;
             });
 
-            $win.on('click.rvsuiSelect' + this.id, function(){
+            $win.on('click.rvsuiSelect' + this.id, function() {
                 self.hideList();
             });
         },
-        destroy: function(){
+        destroy: function() {
             this.$el.remove();
             $win.off('click.rvsuiSelect' + this.id);
         }
